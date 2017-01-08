333d3e2c5f8be0e6e0bc94a8faeff069c689617d
NurAfeeqah <nurafeeqah13291@yahoo.com>
2017-01-08 20:10:09 +0800

    Basic to do list, js file
    Basic to do list, js file

---
+++ main.js
@@ -0,0 +1,26 @@
+dy(function(e){
+    
+
+    $("#addRow").click(function(e){
+        var toDoItem = $("#toDoItem").val();
+
+        var newToDo = "<tr><td>" + toDoItem + "</td><td><input type='checkbox' toDoItem='newToDo'></td></tr>"
+        $("table tbody").append(newToDo);
+    });
+
+    // Find and remove selected table rows
+    $("#deleteRow").click(function(e){
+        $("table tbody").find('input[toDoItem="newToDo"]').each(function(e){
+        	if($(this).is(":checked")){
+                $(this).parents("tr").remove();
+            }
+        });
+    });
+    // Delete all table row except the first row 
+    $('#clearAll').click(function(e){
+        $('#toDoTable').find("tr:gt(0)").remove(); //gt selector
+    });
+
+  
+
+});    
